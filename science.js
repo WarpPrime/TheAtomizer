44a2895d4e5b8a375ba9cf31ca18057e2b651f1e
WarpPrime <77132826+WarpPrime@users.noreply.github.com>
2021-02-05 16:42:23 -0500

    fixed typo

--- science.js
+++ science.js
@@ -10,7 +10,7 @@ function editHBar() {
 	if (darkMatter >= hBarCost) {
 		darkMatter = darkMatter - hBarCost;
 		hTweaks = hTweaks + 1;
-		hBarCost = Math.round(5 * Math.pow(1.1, hTweaks));
+		hBarCost = Math.round(10 * Math.pow(1.1, hTweaks));
 		creationRate = Array.from({length: (hTweaks+1)}, (_, i) => 1/(i + 1)).reduce((a, b) => a + b, 0);
 		qGen = setInterval(q.Quark, Math.round(1000/creationRate));
 	}
