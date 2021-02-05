cdbf02289aee19ed9b32a45f1b4fe993fa50463c
WarpPrime <77132826+WarpPrime@users.noreply.github.com>
2021-02-05 16:13:56 -0500

    science!

---
+++ science.js
@@ -0,0 +1,18 @@
+function makeString() {
+	if (darkMatter >= stringCost) {
+		darkMatter = darkMatter - stringCost;
+		cosmicStrings = cosmicStrings + 1;
+		stringCost = Math.round(5 * Math.pow(1.1, cosmicStrings));
+	}
+}
+
+function editHBar() {
+	if (darkMatter >= hBarCost) {
+		darkMatter = darkMatter - hBarCost;
+		hTweaks = hTweaks + 1;
+		hBarCost = Math.round(5 * Math.pow(1.1, hTweaks));
+		creationRate = Array.from({length: (hTweaks+1)}, (_, i) => 1/(i + 1)).reduce((a, b) => a + b, 0);
+		qGen = setInterval(q.Quark, Math.round(1000/creationRate));
+	}
+
+}
